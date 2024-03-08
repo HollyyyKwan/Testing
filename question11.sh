b02e24c4acac9caec2545b2e0c54d0603f149d0d
乱炖 Holly宝贝 <hollyguan@HollyMacBook-Air.local>
2024-03-08 16:15:13 +1030

    jsj

--- question11.sh
+++ question11.sh
@@ -2,6 +2,10 @@ git checkout -b branch2
 touch file4
 git add file4
 git commit -m "Add file4"
-git checkout main
+echo "Some changes to file4" >> file4
+git add file4
+git commit -m "Modify file4"
 git stash
 git checkout main
+git branch
+git stash list
--- question14.sh
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/ian-knight-uofa/git-practice-04.git
-cd git-practice-04
-git fetch
-git checkout update1
