d2600597d184a26ab263724e5f61c7df486e897e
乱炖 Holly宝贝 <hollyguan@HollyMacBook-Air.local>
2024-03-08 17:08:48 +1030

    jsj

--- question11.sh
+++ question11.sh
@@ -1,9 +1,6 @@
-git checkout -b branch2
-touch file4
-echo "jdfvjj" >> file4
-git add .
-git commit -m "Add file4"
-echo "Some changes to file4" >> file4
-git stash
-git checkout main
-
+git checkout -b homework-branch
+git rm *.sh
+echo "This is a new file for the homework." > file13.txt
+git add file13.txt
+git commit -m "Add file13.txt for homework"
+git push origin homework-branch
