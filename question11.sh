691f994250e568715cbcb852ab7359879e24e610
乱炖 Holly宝贝 <hollyguan@HollyMacBook-Air.local>
2024-03-08 16:19:25 +1030

    jsj

--- question11.sh
+++ question11.sh
@@ -1,11 +1,11 @@
 git checkout -b branch2
 touch file4
+echo "jdfvjj" >> file4
 git add file4
 git commit -m "Add file4"
 echo "Some changes to file4" >> file4
-git add file4
+git add .
 git commit -m "Modify file4"
 git stash
 git checkout main
-git branch
-git stash list
+
